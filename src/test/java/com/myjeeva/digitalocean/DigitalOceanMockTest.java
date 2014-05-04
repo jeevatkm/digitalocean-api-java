9f16d6210c399ae6aaf60d8c431625011823632e
Jeevanandam M <jeeva@myjeeva.com>
2014-05-03 17:28:14 -0700

    Google java code formatter applied

--- src/test/java/com/myjeeva/digitalocean/DigitalOceanMockTest.java
+++ src/test/java/com/myjeeva/digitalocean/DigitalOceanMockTest.java
@@ -19,36 +19,35 @@ import java.net.URL;
 
 @SuppressWarnings("unused")
 public class DigitalOceanMockTest extends TestCase {
-	
-	private @Mocked
-	DefaultHttpClient defaultHttpClient;
 
-	public void testSnapshoWithName() throws Exception {
+  private @Mocked
+  DefaultHttpClient defaultHttpClient;
 
-		new Expectations() {
-			{
-				defaultHttpClient.execute((HttpUriRequest) with(new Object() {
+  public void testSnapshoWithName() throws Exception {
 
-					void validate(HttpUriRequest httpUriRequest)
-							throws MalformedURLException, URISyntaxException {
-						assertEquals(
-								new URL(
-										"https://api.digitalocean.com/droplets/1234/snapshot/?client_id=id&api_key=key&name=snapshot-name")
-										.toURI(), httpUriRequest.getURI());
-					}
-				}));
-				BasicHttpResponse basicHttpResponse = new BasicHttpResponse(
-						HttpVersion.HTTP_1_1, 400, "");
-				JsonObject jsonObject = new JsonObject();
-				jsonObject.add("status", new JsonPrimitive("OK"));
-				basicHttpResponse.setEntity(new StringEntity(jsonObject
-						.toString(), ContentType.APPLICATION_JSON));
-				result = basicHttpResponse;
+    new Expectations() {
+      {
+        defaultHttpClient.execute((HttpUriRequest) with(new Object() {
 
-			}
-		};
-		
-		DigitalOcean digitalOcean = new DigitalOceanClient("id", "key");
-		digitalOcean.takeDropletSnapshot(1234, "snapshot-name");
-	}
+          void validate(HttpUriRequest httpUriRequest) throws MalformedURLException,
+              URISyntaxException {
+            assertEquals(
+                new URL(
+                    "https://api.digitalocean.com/droplets/1234/snapshot/?client_id=id&api_key=key&name=snapshot-name")
+                    .toURI(), httpUriRequest.getURI());
+          }
+        }));
+        BasicHttpResponse basicHttpResponse = new BasicHttpResponse(HttpVersion.HTTP_1_1, 400, "");
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.add("status", new JsonPrimitive("OK"));
+        basicHttpResponse.setEntity(new StringEntity(jsonObject.toString(),
+            ContentType.APPLICATION_JSON));
+        result = basicHttpResponse;
+
+      }
+    };
+
+    DigitalOcean digitalOcean = new DigitalOceanClient("id", "key");
+    digitalOcean.takeDropletSnapshot(1234, "snapshot-name");
+  }
 }
