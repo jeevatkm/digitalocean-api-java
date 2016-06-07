1b8f6915eab1b94810ef24ed50ef83ad1b028965
Jeevanandam M <jeeva@myjeeva.com>
2016-06-07 00:40:25 -0700

    year update

--- src/main/java/com/myjeeva/digitalocean/pojo/Size.java
+++ src/main/java/com/myjeeva/digitalocean/pojo/Size.java
@@ -1,7 +1,7 @@
-/*
+/**
  * The MIT License
  * 
- * Copyright (c) 2010-2015 Jeevanandam M. (myjeeva.com)
+ * Copyright (c) 2013-2016 Jeevanandam M. (myjeeva.com)
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -18,6 +18,7 @@
  * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 package com.myjeeva.digitalocean.pojo;
 
 import java.math.BigDecimal;
